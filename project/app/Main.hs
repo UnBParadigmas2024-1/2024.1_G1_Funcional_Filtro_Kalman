6f7837949044c4528dc2cb9dcf877e12df1ae062
phrezende-eng <ph.hr.001@gmail.com>
2024-04-11 00:28:31 -0300

    fix: just removed functions called into main

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -5,22 +5,22 @@ import Modules
 import Lib
 
 main :: IO ()
-main = do
-  (z1, z2) <- generateMeasurements x_real r1 n
+main = someFunc
+  -- (z1, z2) <- generateMeasurements x_real r1 n
 
-  let (xHat0, p0) = initialization n
-      measurements = zip (toList z1) (toList z2)
-      (xHatFinal, _) = foldl (\(xHat, pPrev) (z1', z2') -> kalmanIteration (xHat, pPrev) (fromList [z1', z2']) 1) (xHat0, p0) measurements
+  -- let (xHat0, p0) = initialization n
+  --     measurements = zip (toList z1) (toList z2)
+  --     (xHatFinal, _) = foldl (\(xHat, pPrev) (z1', z2') -> kalmanIteration (xHat, pPrev) (fromList [z1', z2']) 1) (xHat0, p0) measurements
 
-  putStrLn "Medições do sensor shunt:"
-  print z1
+  -- putStrLn "Medições do sensor shunt:"
+  -- print z1
 
-  putStrLn "Medições do sensor Hall:"
-  print z2
+  -- putStrLn "Medições do sensor Hall:"
+  -- print z2
 
-  putStrLn "Estimativas da corrente elétrica:"
-  print xHatFinal
+  -- putStrLn "Estimativas da corrente elétrica:"
+  -- print xHatFinal
   
-  putStrLn "Covariâncias do erro de estimativa:"
-  print p0
+  -- putStrLn "Covariâncias do erro de estimativa:"
+  -- print p0
   
