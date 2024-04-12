115b87b32b320731e3bb2b88f9e868db03b148ab
Leonardo Takehana <leonardotakehana@gmail.com>
2024-04-12 11:40:04 -0300

    fix: Corrected parameters usage in generateMeasurements Co-authored-by: Paulo Henrique <ph.hr.001@gmail.com>

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -5,22 +5,15 @@ import Modules
 import Lib
 
 main :: IO ()
-main = someFunc
-  -- (z1, z2) <- generateMeasurements x_real r1 n
+main =  do
+  let (x_real', r1', r2', q', n') = initialization 0
+  putStrLn $ "Valor de x_real: " ++ show x_real'
+  putStrLn $ "Valor de r1: " ++ show r1'
+  putStrLn $ "Valor de r2: " ++ show r2'
+  putStrLn $ "Valor de q: " ++ show q'
+  putStrLn $ "Valor de n: " ++ show n'
 
-  -- let (xHat0, p0) = initialization n
-  --     measurements = zip (toList z1) (toList z2)
-  --     (xHatFinal, _) = foldl (\(xHat, pPrev) (z1', z2') -> kalmanIteration (xHat, pPrev) (fromList [z1', z2']) 1) (xHat0, p0) measurements
-
-  -- putStrLn "Medições do sensor shunt:"
-  -- print z1
-
-  -- putStrLn "Medições do sensor Hall:"
-  -- print z2
-
-  -- putStrLn "Estimativas da corrente elétrica:"
-  -- print xHatFinal
-  
-  -- putStrLn "Covariâncias do erro de estimativa:"
-  -- print p0
-  
+  z1 <- generateMeasurements x_real' r1' n' 
+  z2 <- generateMeasurements x_real' r2' n'   
+  putStrLn $ "Medições geradas para z1: " ++ show z1
+  putStrLn $ "Medições geradas para z2: " ++ show z2
--- project/src/Utils.hs
+++ project/src/Utils.hs
@@ -5,33 +5,17 @@ import System.Random
 parseToInt :: String -> Int
 parseToInt str = read str
 
-generateMeasurements :: Double -> [[Double]] -> Int -> IO (Double, Double)
-generateMeasurements xReal r n' = do
-  let r1 = sqrt (r !! 0 !! 0)
-      r2 = sqrt (r !! 1 !! 1)
-  noise1 <- randomRIO (-r1, r1)
-  noise2 <- randomRIO (-r2, r2)
-  let z1 = xReal + noise1
-      z2 = xReal + noise2
-  return (z1, z2)
+initialization :: Int -> (Double, Double, Double, Double, Int)
+initialization _ = (x_real, r1, r2, q, n)
+  where
+    x_real = 10
+    r1 = 0.04
+    r2 = 0.09
+    q = 0.01
+    n = 100
 
-initialization :: Int -> (Double, Double)
-initialization _ = (0, 1)
-
-h :: [[Double]]
-h = [[1, 1]]
-
-r :: [[Double]]
-r = [[0.04, 0], [0, 0.09]] 
-
-kalmanIteration :: (Double, Double) -> (Double, Double) -> Int -> (Double, Double)
-kalmanIteration (xHatPrev, pPrev) (z1, z2) _ =
-  let xHatPred = xHatPrev
-      pPred = pPrev + q
-      z = [z1, z2]
-      k' = map (\row -> map (\val -> val / (r1 + r2)) row) h
-      xHat = xHatPred + sum (zipWith (*) (zipWith (-) z (map (* xHatPred) (head h))) (map (* xHatPred) (head k')))
-      p = (1 - sum (map (sum . map (* xHatPrev)) k')) * pPred
-      q = 0.01 
-      (r1:r2:_) = map sum r
-  in (xHat, p)
+generateMeasurements :: Double -> Double -> Int -> IO [Double]
+generateMeasurements x_real r n = do
+  gen <- newStdGen
+  let randomValues = take n $ randomRs (-r, r) gen
+  return $ map (+ x_real) randomValues
