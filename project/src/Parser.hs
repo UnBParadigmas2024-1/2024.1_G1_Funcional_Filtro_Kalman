03e8c61a1e799dc54c238cdbc78d83c95de31209
diogjunior100 <diogjunior10071@gmail.com>
2024-04-13 18:55:43 -0300

    Modified Parser.hs

--- project/src/Parser.hs
+++ project/src/Parser.hs
@@ -6,11 +6,17 @@ import Data.Csv
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector as V
 
+groupAt :: Int -> [a] -> [[a]]
+groupAt n = go
+  where go [] = []
+        go xs = ys : go zs
+            where ~(ys, zs) = splitAt n xs
 
-parserCsv :: IO ()
+parserCsv :: IO ([[Float]], [[Float]])
 parserCsv = do
     csvData <- BL.readFile "src/shunt.csv"
     case decode NoHeader csvData of
-        Left err -> putStrLn err
-        Right v -> V.forM_ v $ \(x, y) ->
-            putStrLn $ show (x :: Float) ++ " // " ++ show (y :: Float)
+        Left err -> error err
+        --Right v -> V.forM_ v $ \(x, y) ->
+        --    putStrLn $ show (y :: Float) ++ " // " ++ show (y :: Float)
+        Right v -> pure (groupAt 1000 (V.toList (fmap fst v)), groupAt 1000 (V.toList (fmap snd v))) 
