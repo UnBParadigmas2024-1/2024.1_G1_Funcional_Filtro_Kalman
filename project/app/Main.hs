a7d189d18c947656710099746ddfb3b18670eaf9
PauloAbiAcl <pv.silva23@hotmail.com>
2024-04-14 21:06:43 -0300

    Commit inicial

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -1,6 +1,16 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+import Graphics.Vega.VegaLite
 
 main :: IO ()
-main = someFunc
+main = do
+  let dataVals = dataFromColumns []
+        . dataColumn "Gols feitos" (Numbers [3, 2, 4, 5, 6])
+        . dataColumn "Gols sofridos" (Numbers [1, 2, 3, 0, 1])
+      
+      enc = encoding
+        . position X [PName "Gols feitos", PmType Quantitative]
+        . position Y [PName "Gols sofridos", PmType Quantitative]
+         
+  toHtmlFile "jogos.html" $ toVegaLite [width 800, height 800, dataVals [], mark Point [MFilled True], enc [] ]
+
