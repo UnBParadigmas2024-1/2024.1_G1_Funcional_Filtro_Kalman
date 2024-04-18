1274df49f387cc9af57bf97fe4f299b6097e9e11
frmiza <mizzaelsantos1@gmail.com>
2024-04-17 21:34:04 -0300

    apagando comentarios

--- project/app/ChartModule.hs
+++ project/app/ChartModule.hs
@@ -8,21 +8,10 @@ generateChart z1 z2 = do
     let minValz2 = minimum z2 
         maxValz2 = maximum z2
         
-        --inpz1 = [1,2,4,5,6,7,8,9,10] -- entrada dos valores de entrada do programa sem filtragem se necessario
-        --inpz2 = [1.1, 2.4, 3.7, 4.3, 5.6, 6.1, 7.3, 8.6, 9.4, 10.2] -- entrada dos dados sem filtragem, se aplicavel
-        --mininpz2 = minimum inpz2
-        --maxinpz2 = maximum inpz2
-
     -- Definindo os dados
     let dados = dataFromColumns []
           . dataColumn "time" (Numbers z1)
           . dataColumn "valuesOut" (Numbers z2)
-         -- . dataColumn "valuesInp" (Numbers inpz2)
-
-        --encInp = encoding
-        --    . position X [PName "time", PmType Quantitative]
-        --    . position Y [PName "valuesInp", PmType Quantitative, PScale [SDomain (DNumbers [mininpz2, maxinpz2]), SClamp True]]
-        --    . stroke [MString "blue"] -- Definindo a cor da linha dos valores de entrada como azul
 
         encOut = encoding
             . position X [PName "time", PmType Quantitative]
@@ -31,8 +20,7 @@ generateChart z1 z2 = do
 
     toHtmlFile "grafico.html" $ toVegaLite
       [ dados []
-      , vConcat [ --asSpec [mark Line [], encInp []],
-                  asSpec [mark Line [], encOut []]]
+      , vConcat [asSpec [mark Line [], encOut []]]
       , height 500
       , width 1000
       ]
