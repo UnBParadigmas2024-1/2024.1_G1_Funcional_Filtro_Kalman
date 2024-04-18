b3233e61e2381421f5db5292d125ae8d39beecae
frmiza <mizzaelsantos1@gmail.com>
2024-04-18 16:49:45 -0300

    adicionado funcao de plotagem a utils

--- project/src/Utils.hs
+++ project/src/Utils.hs
@@ -45,6 +45,11 @@ import qualified Data.Vector as Vector
 import qualified Data.Csv as Cassava
 import Data.Int (Int)
 
+-- chart 
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
+import Control.Monad (void)
+
 -- Definição de dados
 data Item =
   Item
@@ -255,6 +260,26 @@ desvioPadraoAgrupamento lista = sqrt (sum (map (\x -> (x - mediaAritmetica lista
 varianciaReal :: Double -> IO (Double)
 varianciaReal variancia = do 
     return (variancia ^ 2)
+
+renderGraf :: [Double] -> [Double] -> Renderable ()
+renderGraf z1 z2 = toRenderable layout
+  where
+    val = plot_lines_style . line_color .~ opaque blue
+          $ plot_lines_values .~ [ zip z1 z2 ]
+          $ plot_lines_title .~ "valores"
+          $ def
+
+    layout = layout_title .~ "Resultados"
+      $ layout_grid_last .~ False
+      $ layout_plots .~ [toPlot val]
+      $ def
+
+saveGraf :: Renderable a -> Maybe String -> Maybe String -> IO () -- recebe o grafico renderizado e o nome do arquivo + formato
+saveGraf graf nm fmt = do
+  namedGraf (maybe "grafico" id nm) (maybe "svg" id fmt) graf
+  where
+    namedGraf :: String -> String -> Renderable a -> IO ()
+    namedGraf sNm sFmt grf = void $ renderableToFile def ("img/" ++ sNm ++ "." ++ sFmt) grf
 
 showStatistics :: Double -> Double -> Double -> Double -> Double -> Double -> IO()   
 showStatistics media1 desvio1 variancia1 media2 desvio2 variancia2 = do
